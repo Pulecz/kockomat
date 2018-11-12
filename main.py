b5bbba8473750d38b03ea2c93dadc535e4ea3a3f
tmikulec <tmikulec@redhat.com>
2018-11-12 19:31:36 +0100

    appending to the db with a BUG, and don't put the db to git

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 import praw  # for reddit API
 import pickle  # for pickleRICK
-import our_secrets
 from os.path import exists as does_file_exist  # for checking if file exist
 try:
     import our_secrets
 except ImportError:
     print('ha! you are missing the secretsssssss')
     exit(1)  # exit with errrcode 1
+
+# Define data structure
+# lists for multiple submissions
+list_of_data = []
 
 # make an instance from Reddit class defined in praw module
 # username and password is for write access, rest is for read only
@@ -17,20 +20,26 @@ reddit = praw.Reddit(client_id=our_secrets.client_id,
                      username=our_secrets.username,
                      password=our_secrets.password)
 
+# TODO when no internet connectivity (or train wifi with long timeouts) skip getting the data and quit gracefully
 # work with A subreddit
 for submission in reddit.subreddit('documentaries').hot(limit=25):
     print(submission.title)
-
-
-# TODO3 (hard) - append to the database:
-# OR
-# TODO3 (easier) - overwrite the database:
-data = {'title': submission.title,
+    # dictionary for details
+    data = {'title': submission.title,
         'theURL':submission.url, 
-		"score" : submission.ups}
+	"score" : submission.ups}
+    list_of_data.append(data)
+
+# in first run, database.db does not exist, skip reading and appending
 if does_file_exist('database.db'):
-    pass  # file exists, do nothing
-    print('WARNING: \'databse.db\' exists, not writing anything')
-else:
-    with open('database.db', 'wb') as iowrap:  # write as bytes
-        pickle.dump(data, iowrap)  # save the data to iowrap instance
+    # file exists, read the db, and load the data
+    with open('database.db', 'rb') as iowrap:  # write as bytes
+        old_list_of_data = pickle.load(iowrap)  # load the data from iowrap instance
+    print('INFO: Loaded \'databse.db\'')
+    # put the old_list_of_data together with the new data
+    # TODO don't duplicate the data, tip use sets to filter, based on some unique information in submission
+    list_of_data = old_list_of_data + list_of_data
+# write the db
+with open('database.db', 'wb') as iowrap:  # write as bytes
+    pickle.dump(list_of_data, iowrap)  # save the data to iowrap instance
+    print('INFO: Saved {0} items \'databse.db\''.format(len(list_of_data)))
